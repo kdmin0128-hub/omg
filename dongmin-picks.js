6a2521a96064ff832a34a4c7d033a391de55fd2e
kdmin0128-hub <kdmin0128@gmail.com>
2026-06-21 15:33:02 +0900

    Add files via upload

---
+++ dongmin-picks.js
@@ -0,0 +1,32 @@
+const dongminPicks = [
+    {
+        name: "선희네",
+        menu: "보쌈, 잔치국수",
+        img: "https://img1.kakaocdn.net/cthumb/local/C800x800.q50/?fname=https%3A%2F%2Fpostfiles.pstatic.net%2FMjAyNDExMjRfMTEz%2FMDAxNzMyNDQ3MDY4MjQy.qRgqeoaqwASXzkG2dkML6_idYAQPCjwUszc6kSCRw54g.Y0ozQadLBDMPg6Sp14VRDRCNuSJ6boiv5KNcW3kEIZgg.JPEG%2FKakaoTalk_20241124_201008658_07.jpg%3Ftype%3Dw966", 
+        comment: "서울에 이런 곳이 있었나 싶은 지하 던전에서 맛보는 부드러운 수육과 깊고 진한 육수의 잔치국수!",
+        tags: ["잔치국수 4천원", "어른들의 놀이터", "내가 제일 어림", "계좌이체/현금만"],
+        x: "", y: "",
+        kakaoId: "653484290",  
+        naverId: "1770707904"   
+    },
+    {
+        name: "합천돼지국밥",
+        menu: "돼지국밥, 수육정식",
+        img: "https://img1.kakaocdn.net/cthumb/local/C800x800.q50/?fname=https%3A%2F%2Fpostfiles.pstatic.net%2FMjAyNjAxMjZfMTM1%2FMDAxNzY5NDA3NDkyOTEw.4mvpUtZrdmuoatLSQrahQqNtdBz76VC8lplrtmWyPqwg.hNFHaBgn08mqBtKNvcda1gY8pOzCGy-9Wuh3NVYGV-Ag.JPEG%2FIMG%25EF%25BC%25BF5364.JPG%3Ftype%3Dw966", 
+        comment: "부산보다 더 맛있는 진짜 돼지국밥. ",
+        tags: ["잡내없음", "오늘은 내가 국밥부장관", "합석가능"],
+        x: "", y: "",
+        kakaoId: "1630483489",
+        naverId: "1636084766"
+    },
+    {
+        name: "소문난마산아구",
+        menu: "찜정식(점심특선)",
+        img: "https://img1.kakaocdn.net/cthumb/local/C800x800.q50/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flocal%2FkakaomapPhoto%2Freview%2F53613156066953a85c2a45c2e29286aaa227119a%3Foriginal",
+        comment: "인당 1만원 초반대로 점심에 아구찜을 먹을 수 있는 곳. 인원이 많다면 알+고니를 추가해보자",
+        tags: ["미친가성비", "아는맛 is 존맛", "No 볶음밥 No life"],
+        x: "", y: "",
+        kakaoId: "1694116928",
+        naverId: "37852829"
+    }
+];
